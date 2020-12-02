8b262d489d11203b67d9fe1d04d6f959b22c101c
Ulises Rojas González <ulises.rogo@gmail.com>
2020-12-01 19:46:00 -0600

    Add examples of using Pipes

--- src/app/components/product.component.ts
+++ src/app/components/product.component.ts
@@ -30,6 +30,8 @@ export class ProductComponent
   // @Input() product?: Product;
   @Output() productClicked: EventEmitter<any> = new EventEmitter();
 
+  today = new Date();
+
   constructor() {
     console.log('1. cycle constructor');
   }
