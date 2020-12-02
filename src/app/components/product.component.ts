4c2ae88b3da92e9a2fe877b9c5856385cd0616d1
Ulises Rojas González <ulises.rogo@gmail.com>
2020-12-01 18:15:26 -0600

    Add examples of the components lifecycle events

--- src/app/components/product.component.ts
+++ src/app/components/product.component.ts
@@ -1,12 +1,21 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  OnChanges,
+  SimpleChanges,
+  OnInit,
+  DoCheck,
+  OnDestroy,
+} from '@angular/core';
 import { Product } from '../product.model';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
 })
-
-export class ProductComponent {
+export class ProductComponent implements OnChanges, OnInit, /*DoCheck,*/ OnDestroy {
   // since it is asking to initalize this member...
   @Input() product: Product = {
     id: '',
@@ -18,6 +27,26 @@ export class ProductComponent {
   // or...
   // @Input() product?: Product;
   @Output() productClicked: EventEmitter<any> = new EventEmitter();
+
+  constructor() {
+    console.log('1. cycle constructor');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('2. cycle ngOnChanges', changes);
+  }
+
+  ngOnInit(): void {
+    console.log('3. cycle ngOnInit');
+  }
+
+  // ngDoCheck(): void {
+  //   console.log('4. cycle ngDoCheck');
+  // } // Error Declaring ngDoCheck and ngOnChanges method in a class is not recommended
+
+  ngOnDestroy(): void {
+    console.log('5. cycle ngOnDestroy');
+  }
 
   addToCart(): void {
     console.log('added to cart');
