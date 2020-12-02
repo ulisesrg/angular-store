7f52e186a8ebef26f9eeb50f9249f8ffaa3b6a43
Ulises Rojas González <ulises.rogo@gmail.com>
2020-12-02 10:58:42 -0600

    Add first customizations on Home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+
+import Swiper from 'swiper';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
+
+  mySwiper: Swiper = new Swiper('', {});
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    this.mySwiper = new Swiper('.swiper-container', {});
   }
 
 }
