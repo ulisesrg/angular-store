7f614b2cbd70d05dcf25b47bfdcd8dfa3ceaabf8
Ulises Rojas González <ulises.rogo@gmail.com>
2020-12-01 18:35:58 -0600

    Add general styles and component styles through ':host' selector

--- src/app/components/product.component.ts
+++ src/app/components/product.component.ts
@@ -14,8 +14,10 @@ import { Product } from '../product.model';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss'],
 })
-export class ProductComponent implements OnChanges, OnInit, /*DoCheck,*/ OnDestroy {
+export class ProductComponent
+  implements OnChanges, OnInit, /*DoCheck,*/ OnDestroy {
   // since it is asking to initalize this member...
   @Input() product: Product = {
     id: '',
