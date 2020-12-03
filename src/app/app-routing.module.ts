156ef262aeb84988b176a82f9edf54bfb7b11fe7
Ulises Rojas González <ulises.rogo@gmail.com>
2020-12-03 13:10:17 -0600

    Add an Admin view through Angular Schematic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,11 @@ const routes: Routes = [
     loadChildren: () => import('./demo/demo.module').then((m) => m.DemoModule),
   },
   {
+    path: 'admin',
+    loadChildren: () =>
+      import('./admin/admin.module').then((m) => m.AdminModule),
+  },
+  {
     path: '**',
     loadChildren: () =>
       import('./page-not-found/page-not-found.module').then(
